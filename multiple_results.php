2f44bda479faa2a5d7455b2c5dea8c354dd8cbb5
rweiner <rachel.g.weiner@gmail.com>
2014-05-24 21:55:50 -0700

    front end update

--- multiple_results.php
+++ multiple_results.php
@@ -1,18 +1,21 @@
 <?php
+	require_once('TwitterAPIExchange.php');		//loads the php file
+	require_once('OAuth.php');
+	require_once('yelpTest.php');
+	require_once('twitterCall.php');
+	
 	if (isset($_POST)) {
 		if ($_POST['location'] == null) 
 			$errorArray[] = "Please enter a location.";
-		else {
-			$form_location = $_POST['location'];
-			//$base='https://maps.googleapis.com/maps/api/geocode/json?address=';
-			//$location = explode(' ',$form_location);
-			//$query =implode("+",$location);
-			//$option='sensor=false';
-			
-		}
-		if (count($errorArray) == 0){
-			header("Location: http://localhost/restaurantzz/multiple_results.php");
-			die();
+		elseif($_POST['latitude'] == null){
+			$errorArray[]= "lat missing";
+		}else{
+			$lat = $_POST['latitude'];
+			$long = $_POST['longitude'];	
+			//$errorArray[] = $lat;
+			//$errorArray[] = $long;
+			//header("Location: index.php?lat=".$lat."&long=".$long);	
+			//die();
 		}
 		
 	}
--- test.php
+++ test.php
@@ -1,76 +1,32 @@
-<!DOCTYPE html>
-<html>
-	<head>
-
-	</head>
-	<body>
-
-	<?php
-		
-		require_once('TwitterAPIExchange.php');		//loads the php file
-		require_once('OAuth.php');
-		require_once('yelpTest.php');
-		require_once('twitterCall.php');
-		
-		$list = yelp(33.8741025, -117.7364668);
-		$list = twitter($list, 33.8741025, -117.7364668);
-		
-		for($i = 0; $i < 40; $i++)
+<?php
+	
+	require_once('TwitterAPIExchange.php');		//loads the php file
+	require_once('OAuth.php');
+	require_once('yelpTest.php');
+	require_once('twitterCall.php');
+	
+	$list = yelp(33.8741025, -117.7364668);
+	$list = twitter($list, 33.8741025, -117.7364668);
+	
+	for($i = 0; $i < 40; $i++)
+	{
+		for($j = 0; $j < 39 - $i; $j++)
 		{
-			for($j = 0; $j < 39 - $i; $j++)
+			if( $list[$j]["score"] < $list[$j+1]["score"]  )
 			{
-				if( $list[$j]["score"] < $list[$j+1]["score"]  )
-				{
-					$swap = $list[$j];
-					$list[$j] = $list[$j+1];
-					$list[$j+1] = $swap;
-				}
+				$swap = $list[$j];
+				$list[$j] = $list[$j+1];
+				$list[$j+1] = $swap;
 			}
 		}
-		
-		
-		
-		for ($i = 0; $i < 40; $i++){
-			echo $list[$i]["name"].'</br>';
-			echo $list[$i]["hashtag"].'</br>';
-			echo $list[$i]["address"].'</br>';
-			echo $list[$i]["url"].'</br>';
-			echo $list[$i]["score"].'</br>'.'</br>';
-		}
-		
-		//determine highest tweets
-		/*$highest = '0';
-		for ($i = 0; $i < 40; $i++){
-			if ($list[$i]["score"] > $highest){
-				$highest = $list[$i]["score"]; 	
-			}
-		}
-		echo $highest;*/
-		
-		/*$temp = array();
-		$tempcounter = 0;
-		while ($highest < 0){
-			for ($i = 0; $i < 40; $i++){
-				if ($list[$i]["score"] == $highest){
-					$temp[$tempcounter] = $list[$i];
-					$tempcounter = $tempcounter + 1;
-				}
-				$highest = $higest - 1;
-			}
-		}
-		
-		for ($i = 0; $i < 40; $i++){
-			echo $temp[$i]["name"].'</br>';
-			echo $temp[$i]["hashtag"].'</br>';
-			echo $temp[$i]["address"].'</br>';
-			echo $temp[$i]["url"].'</br>';
-			echo $temp[$i]["score"].'</br>'.'</br>';
-		}*/
-		
-		
-		
-		
-		
-	?>
-	</body>
-</html>
+	}
+	
+	//print for debugging us only
+	/*for ($i = 0; $i < 40; $i++){
+		echo $list[$i]["name"].'</br>';
+		echo $list[$i]["hashtag"].'</br>';
+		echo $list[$i]["address"].'</br>';
+		echo $list[$i]["url"].'</br>';
+		echo $list[$i]["score"].'</br>'.'</br>';
+	}*/	
+?>
