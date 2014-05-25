3659b7c53d593b5161342970d9541e653d2fe131
Grace Pai <gpai@uci.edu>
2014-05-24 17:38:14 -0700

    front end update- don't freak out, it's a lot, I know

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
+$results = array(
+		array(
+			"name"=>"Outback Steakhouse",
+			"hashtag" =>"OutbackSteakhouse",
+			"address" =>"15433 Culver Dr Irvine, CA 92604",
+			"url" =>"www.outback.com",
+			"score" => 4
+		),
+		array(
+			"name"=>"BJs",
+			"hashtag" =>"BJs",
+			"address" =>"13130 Jamboree Rd Irvine, CA 92602",
+			"url" =>"www.bjrestaurants.com",
+			"score" => 3
+		),
+		array(
+			"name"=>"Outback Steakhouse",
+			"hashtag" =>"OutbackSteakhouse",
+			"address" =>"15433 Culver Dr Irvine, CA 92604",
+			"url" =>"www.outback.com",
+			"score" => 2
+		),
+		array(
+			"name"=>"Little Sheep Mongolian Hotpot",
+			"hashtag" =>"LittleSheepMongolianHotpot",
+			"address" =>"15361 Culver Dr Irvine, CA 92604",
+			"url" =>"www.littlesheephotpot.com",
+			"score" => 1
+		),
+	);
 	include 'index.phtml';
 ?>
