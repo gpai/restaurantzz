2afecda14a9b2f6b221492e159d511c88e25f1b8
rweiner <rachel.g.weiner@gmail.com>
2014-05-24 22:13:29 -0700

    input read and converted to long/lat coordinates

--- multiple_results.php
+++ multiple_results.php
@@ -7,17 +7,18 @@
 	if (isset($_POST)) {
 		if ($_POST['location'] == null) 
 			$errorArray[] = "Please enter a location.";
-		elseif($_POST['latitude'] == null){
-			$errorArray[]= "lat missing";
 		}else{
-			$lat = $_POST['latitude'];
-			$long = $_POST['longitude'];	
+			//$lat = $_POST['latitude'];
+			//$long = $_POST['longitude'];	
 			//$errorArray[] = $lat;
 			//$errorArray[] = $long;
-			//header("Location: index.php?lat=".$lat."&long=".$long);	
-			//die();
+			console.log('it works!!!!');
 		}
-		
+	if(count($errorArray)==0){
+		header("Location: index.php?lat=".$lat."&long=".$long);	
+		die();
 	}
+		
+	
 	include 'multiple_results.phtml';
 ?>
