bc2474aa1689cc4f300f2eaa6fdb72ed159cf1e0
Grace Pai <gpai@uci.edu>
2014-05-24 20:36:26 -0700

    push

--- multiple_results.php
+++ multiple_results.php
@@ -2,11 +2,16 @@
 	if (isset($_POST)) {
 		if ($_POST['location'] == null) 
 			$errorArray[] = "Please enter a location.";
-		else 
+		else {
 			$form_location = $_POST['location'];
-		
+			//$base='https://maps.googleapis.com/maps/api/geocode/json?address=';
+			//$location = explode(' ',$form_location);
+			//$query =implode("+",$location);
+			//$option='sensor=false';
+			
+		}
 		if (count($errorArray) == 0){
-			header("Location: http://localhost/restaurantzz/index.php");
+			header("Location: http://localhost/restaurantzz/multiple_results.php");
 			die();
 		}
 		
